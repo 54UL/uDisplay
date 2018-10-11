86960258a46b5f7d4704a91d5758487bf1366c49
Saul Aceves Montes <54ulxd@gmail.com>
2018-10-10 22:00:03 -0500

    added basic example
    just a simple exampe for the at89s52 device

---
+++ Examples/HelloWorld.c
@@ -0,0 +1,25 @@
+
+
+#include "Oled_1306Driver.h"
+
+#include <AT89X52.h>
+
+
+
+
+
+
+
+void main ()
+{
+		
+
+  		OLED_Initialize();
+	    OLED_cls();
+		OLED_gotoxy(0,0);
+	    OLED_DrawString("hello world");
+	 	OLED_gotoxy(1,0);
+	    OLED_DrawString(":3");
+
+
+}
