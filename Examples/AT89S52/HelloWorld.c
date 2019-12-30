d10f2dc49e5a8fabfc0916ee6386710dc8efa919
Saul Acvs <54ulxd@gmail.com>
2019-12-30 11:48:17 -0600

    identation

--- Examples/AT89S52/HelloWorld.c
+++ Examples/AT89S52/HelloWorld.c
@@ -1,25 +1,12 @@
-
-
 #include "Oled_1306Driver.h"
-
 #include <AT89X52.h>
-
-
-
-
-
-
 
 void main ()
 {
-		
-
-  		OLED_Initialize();
-	    OLED_cls();
-		OLED_gotoxy(0,0);
-	    OLED_DrawString("hello world");
-	 	OLED_gotoxy(1,0);
-	    OLED_DrawString(":3");
-
-
+	OLED_Initialize();
+	OLED_cls();
+	OLED_gotoxy(0,0);
+	OLED_DrawString("hello world");
+	OLED_gotoxy(1,0);
+	OLED_DrawString(":3");
 }
